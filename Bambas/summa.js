26745a3c288461a088bfd702ad748564d3388051
snejana88 <73829049+snejana88@users.noreply.github.com>
2021-04-15 11:24:39 +0200

    catalogo mas fotos

---
+++ Bambas/summa.js
@@ -0,0 +1,35 @@
+// loop 
+
+let hru = [2,43,18,9,3,7,19]
+let l = hru.length
+
+console.log(hru.length)
+
+ let sum = hru.reduce(function(total, amount){
+     return total + amount
+ })
+ 
+ console.log(sum)
+
+
+ let sum1 = hru.reduce(function(sum, current){
+    return sum + current
+})
+
+console.log(sum1)
+
+
+//  let sum = arr.reduce((total, amount)
+//     => total + amount)
+
+
+deberes para mañana: FUNCIÓN:
+
+1. que te permita sumar ÚNICAMENTE los números pares dentro del array, y;
+
+2. que te permita introducir un número en un array (da igual si es al principio o al final del array)
+
+
+  
+
+
